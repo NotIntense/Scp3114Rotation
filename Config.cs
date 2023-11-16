6cfdeeb6a643cf609c40f55810da48033ca57920
NotIntense <mbro8210@gmail.com>
2023-11-15 23:05:15 -0500

    Add project files.

---
+++ Config.cs
@@ -0,0 +1,17 @@
+﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
+
+namespace Scp3114Rotation
+{
+    public class Config : IConfig
+    {
+        [Description("Gets or sets if the plugin is enabled.")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Gets or sets if the plugin's debug logs are enabled.")]
+        public bool Debug { get; set; } = false;
+
+        [Description("Gets or sets a % chance SCP-106 has to be replaced by SCP-3114.")]
+        public int ReplacementChance { get; set; } = 50;
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,38 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using Exiled.Loader;
+
+namespace Scp3114Rotation
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Author { get; } = "NotIntense";
+        public override string Name { get; } = "SCP-3114Rotation";
+        public override bool IgnoreRequiredVersionCheck { get; } = true;
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.Spawned += OnSpawn;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.Spawned -= OnSpawn;
+            base.OnDisabled();
+        }
+
+        public void OnSpawn(SpawnedEventArgs ev)
+        {
+            if(ev.Reason == Exiled.API.Enums.SpawnReason.RoundStart 
+                && ev.Player.Role.Type == PlayerRoles.RoleTypeId.Scp106 
+                && Player.List.Where(x => x.Role.Type == PlayerRoles.RoleTypeId.Scp3114).Count() < 1)
+            {
+                int chance = Loader.Random.Next(0, 101);
+
+                if (chance <= Config.ReplacementChance)
+                    ev.Player.Role.Set(PlayerRoles.RoleTypeId.Scp3114);
+            }
+        }
+    }
+}
